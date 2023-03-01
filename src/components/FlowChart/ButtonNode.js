fb481c1f1e53b767e1a40a675d624387727c3850
Alisa Promthep <alisa.promthep@gmail.com>
2023-03-01 16:31:00 +0700

    added css animationn to flowchart buttons (expand when hover)

--- src/components/FlowChart/ButtonNode.js
+++ src/components/FlowChart/ButtonNode.js
@@ -1,9 +1,7 @@
 import React, {useCallback} from 'react';
 import {Handle, Position} from 'reactflow';
 import './FlowChart.scss';
-import Tooltip from '../Tooltip/Tooltip';
 import Popup from 'reactjs-popup';
-import {FaRegHandPointLeft} from 'react-icons/fa'
 
 
 
@@ -14,15 +12,7 @@ const ButtonNode = ({id,data}) => {
     return (
         <>
             <Handle type="source" position={Position.Bottom}/>
-            <Popup
-            trigger={ <button  className={`flowchart__button flowchart__${data.level}`} onClick={handleClick}>{data.label}</button>
-            }
-            on={['hover']}
-            position='bottom center'
-            >
-                <p>Click me</p>
-            </Popup>
-            <FaRegHandPointLeft className='tooltip__button'/>
+                <button  className={`flowchart__button flowchart__${data.level}`} onClick={handleClick}>{data.label}</button>
             <Handle type="target" position={Position.Top}/>
         </>
     )
