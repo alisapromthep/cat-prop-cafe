e2c9b2c4794cb96b9ea6c94200618579fa5cfe35
Alisa Promthep <alisa.promthep@gmail.com>
2023-03-15 21:33:29 +0700

    bolded and remove underline from links in the footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -7,14 +7,15 @@ const Footer = () => {
     return (
         <div className='footer'>
             <p>Built with React</p>
-            <Link to='/attributes'>Icons Attributes</Link>
+            <Link className='footer__link' to='/attributes'>Icons Attributes</Link>
             <div className='footer__bottom-container'>
                 <FaLinkedin className='footer__icons'/>
                 <FaGithubSquare className='footer__icons'/>
             </div>
             <p>Made with ♡ by <a href='https://alisapromthep.dev'
-            target="_blank"
-            rel="noreferer noopener"
+            target='_blank'
+            rel='noreferer noopener'
+            className='footer__link'
             >Alisa</a></p>
         </div>
     )
