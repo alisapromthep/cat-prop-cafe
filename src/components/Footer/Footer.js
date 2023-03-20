0aeb91aa9b3bb1c193edc38a8bdc6a00a92b53d6
Alisa Promthep <alisa.promthep@gmail.com>
2023-03-20 21:13:43 +0700

    footer re-arrangement

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -6,17 +6,21 @@ import {FaLinkedin,FaGithubSquare} from 'react-icons/fa';
 const Footer = () => {
     return (
         <div className='footer'>
-            <p>Built with React</p>
-            <Link className='footer__link' to='/attributes'>Icons Attributes</Link>
-            <div className='footer__bottom-container'>
-                <FaLinkedin className='footer__icons'/>
-                <FaGithubSquare className='footer__icons'/>
+            <div className='footer__left'>
+                <p>Built with React</p>
+                <Link className='footer__link' to='/attributes'>Icons Attributes</Link>
             </div>
-            <p>Made with ♡ by <a href='https://alisapromthep.dev'
-            target='_blank'
-            rel='noreferer noopener'
-            className='footer__link'
-            >Alisa</a></p>
+            <div className='footer__right'>
+                <div className='footer__icons-container'>
+                    <FaLinkedin className='footer__icons'/>
+                    <FaGithubSquare className='footer__icons'/>
+                </div>
+                <p>Made with ♡ by <a href='https://alisapromthep.dev'
+                target='_blank'
+                rel='noreferer noopener'
+                className='footer__link'
+                >Alisa</a></p>
+            </div>
         </div>
     )
 }
