34948d57b50e7a8249cd51fd19974a307fd41b23
Alisa Promthep <alisa.promthep@gmail.com>
2023-03-21 21:51:55 +0700

    make logo link back to homepage

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import './NavBar.scss';
-import Popup from 'reactjs-popup';
+import {Link} from 'react-router-dom';
 import paw from '../../assets/icons/paw.png';
 
 
 function NavBar() {
     return (
         <div className='nav'>
+            <Link to='/'>
             <img src={paw} alt='cat paw logo' className='cat'/>
+            </Link>
         </div>
     )
 }
