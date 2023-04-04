3e7102e192a36dbd3fd6fbc70d1fbebf86f0996d
Alisa Promthep <alisa.promthep@gmail.com>
2023-04-04 22:05:49 +0700

    fix tutorial modal, added close button, and styled tutorial button

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import Popup from 'reactjs-popup';
 import tutorial from '../../data/tutorial.json';
-
+import {FaWindowClose} from 'react-icons/fa';
+import './Modal.scss';
 const Modal = () => {
 
     return (
@@ -15,20 +16,29 @@ const Modal = () => {
         nested
         role='dialog'
         >
-            <div className='nav__tutorial-container'>
-                <h1>Tutorial</h1>
-                {
-                    tutorial.map((step)=>{
-                        return (
-                            <p key={step.id}
-                            className='tutorial__step'>
-                                {step.id}.
-                                {step.content}
-                            </p>
-                        )
-                    })
-                }
-            </div>
+            {close => (
+                <div className='modal__tutorial-container'>
+                    <div className='modal__tutorial'>
+                    <div>
+                        <h1 className='modal__title'>Tutorial</h1>
+                        {
+                            tutorial.map((step)=>{
+                                return (
+                                    <p key={step.id}
+                                    className='tutorial__step'>
+                                        {step.id}.
+                                        {step.content}
+                                    </p>
+                                )
+                            })
+                        }
+                    </div>
+                    <button className='modal__button'>
+                        <FaWindowClose className='modal__close-icon' onClick={close}/>
+                    </button>
+                    </div>
+                </div>
+            )}
         </Popup>
     )
 }
