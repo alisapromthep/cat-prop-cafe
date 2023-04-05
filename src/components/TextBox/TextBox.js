b37f7e0fd9ba8fff7caa0f733e01b144ca056ee9
Alisa Promthep <alisa.promthep@gmail.com>
2023-04-05 22:59:55 +0700

    created a new textbox with cat component for reuseability

---
+++ src/components/TextBox/TextBox.js
@@ -0,0 +1,57 @@
+import './TextBox.scss'; 
+import {Link} from 'react-router-dom';
+import greycat from '../../assets/icons/greycat.png';
+import catinbox from '../../assets/icons/catinbox.png';
+import orangecat from '../../assets/icons/orangecat.png';
+import stretchcat from '../../assets/icons/stretchcat.png';
+
+
+function TextBox({title, text, linkTo, linkText}) {
+
+    const cats = [
+        {
+            src: greycat,
+            alt: 'a grey cat'
+        },
+        {
+            src: catinbox,
+            alt: 'a cat in a box'
+        },
+        {
+            src: orangecat,
+            alt: 'an orange cat'
+        },
+        {
+            src: stretchcat,
+            alt: 'a cat stretching'
+        },
+
+    ];
+
+    return (
+        <div className='container__context'>
+            <h1 className='container__title'>{title}</h1>
+            <div className='container__rightbox'>
+                <div className='container__textbox'>
+                    <p className='container__text'>
+                        {text}
+                    </p>
+                    <Link to={linkTo} className='link'> 
+                        {linkText}
+                    </Link>
+                </div>
+                <div className='cat-container'>
+                    {
+                        cats.map((cat,i)=>{
+                            return (
+                                <img key={i} src={cat.src} alt={cat.alt} className='cat'/>
+                            )
+                        })
+                    }
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default TextBox
--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,52 +1,20 @@
 import React from 'react';
 import './HomePage.scss';
-import {Link} from 'react-router-dom';
-import greycat from '../../assets/icons/greycat.png';
-import catinbox from '../../assets/icons/catinbox.png';
-import orangecat from '../../assets/icons/orangecat.png';
-import stretchcat from '../../assets/icons/stretchcat.png';
+import TextBox from '../../components/TextBox/TextBox.js';
+
 
 const HomePage = () => {
 
-const cats = [
-        {
-            src: greycat,
-            alt: 'a grey cat'
-        },
-        {
-            src: catinbox,
-            alt: 'a cat in a box'
-        },
-        {
-            src: orangecat,
-            alt: 'an orange cat'
-        },
-        {
-            src: stretchcat,
-            alt: 'a cat stretching'
-        },
-
-    ];
+    const aboutText = "The first time I learn about React props, I was very confuse, and found it very difficult to visualize how the information was getting pass around. Thus, I decided to create this little game, to help other new developer like me visualize and learn props."
 
     return (
         <div className='home__container'>
-            <div className='home'>
-                <h1 className='home__cafe-name'>Welcome to Cat Prop Cafe</h1>
-                <div className='home__welcome'>
-                    <p className='home__about-text'>
-                        The first time I learn about React props, I was very confuse, and found it very difficult to visualize how the information was getting pass around. Thus, I decided to create this little game, to help other new developer like me visualize and learn props.</p>
-                </div>
-                    <Link to="/game" className='home__start-button'>start</Link>
-                <div className='home__cat-container'>
-                    {
-                        cats.map((cat,i)=>{
-                            return (
-                                <img key={i} src={cat.src} alt={cat.alt} className='cat'/>
-                            )
-                        })
-                    }
-                </div>
-            </div>
+            <TextBox
+            title="Welcome to Cat Prop Cafe"
+            text={aboutText}
+            linkTo="./game"
+            linkText="start"
+            />
         </div>
     )
 }
